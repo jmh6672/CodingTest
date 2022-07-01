37830f50b598f53f24c6bcac5c42504c75321156
정명한 <>
2022-07-01 22:54:56 +0900

    이분탐색, 완전탐색 다시 풀기

--- src/main/java/programers/binarySearch/Test1.java
+++ src/main/java/programers/binarySearch/Test1.java
@@ -1,35 +1,37 @@
 package programers.binarySearch;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Test1 {
-    static final int CASE_COUNT = 1000000000;
+    static final int CASE_COUNT = 10000;
+
     static final int CASE_COUNT_ARRAY = 100000;
+
 
     /**
      문제 설명
-     수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+     n명이 입국심사를 위해 줄을 서서 기다리고 있습니다. 각 입국심사대에 있는 심사관마다 심사하는데 걸리는 시간은 다릅니다.
 
-     1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-     2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-     3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+     처음에 모든 심사대는 비어있습니다. 한 심사대에서는 동시에 한 명만 심사를 할 수 있습니다. 가장 앞에 서 있는 사람은 비어 있는 심사대로 가서 심사를 받을 수 있습니다. 하지만 더 빨리 끝나는 심사대가 있으면 기다렸다가 그곳으로 가서 심사를 받을 수도 있습니다.
 
-     1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+     모든 사람이 심사를 받는데 걸리는 시간을 최소로 하고 싶습니다.
 
-     제한 조건
-     시험은 최대 10,000 문제로 구성되어있습니다.
-     문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
-     가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+     입국심사를 기다리는 사람 수 n, 각 심사관이 한 명을 심사하는데 걸리는 시간이 담긴 배열 times가 매개변수로 주어질 때, 모든 사람이 심사를 받는데 걸리는 시간의 최솟값을 return 하도록 solution 함수를 작성해주세요.
+
+     제한사항
+     입국심사를 기다리는 사람은 1명 이상 1,000,000,000명 이하입니다.
+     각 심사관이 한 명을 심사하는데 걸리는 시간은 1분 이상 1,000,000,000분 이하입니다.
+     심사관은 1명 이상 100,000명 이하입니다.
      * */
     public static void main(String[] args) {
         Test1 test1 = new Test1();
+        Random random = new Random();
 
         List<Integer> times = new ArrayList<>();
 
-        Random random = new Random();
+
         for(int i=0; i<CASE_COUNT_ARRAY; i++){
             int pick = random.nextInt(5)+1;
 
@@ -37,34 +39,20 @@ public class Test1 {
         }
 
         long startTime = System.currentTimeMillis();
-        long result = test1.solution(
+        long result = test1.solution1(
                 random.nextInt(CASE_COUNT)+1,
                 times.stream().mapToInt(Integer::intValue).toArray()
         );
+
         System.out.println(String.format("Spent Time : %s(ms)",System.currentTimeMillis()-startTime));
         System.out.println(result);
     }
 
-    public long solution(int n, int[] times) {
+    public long solution1(int n, int[] times) {
         long answer = 0;
-        Arrays.sort(times);
 
-        long left = 0;
-        long right = (long) n * times[times.length - 1]; //가장 최악의 경우의(오래걸리는) 시간
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long sum = 0; // 총 심사한 인원
-            for (int i = 0; i < times.length; i++) { //빨리 심사하는 심사관 순으로 심사처리
-                sum += mid / times[i];
-            }
-            if (sum < n) { // 해야할 인원보다 심사처리 못함 -> 시간 더 필요
-                left = mid + 1;
-            } else { // 해야할 인원보다 심사처리 많이함 -> 시간을 줄여서 더 최고 경우의 시간을 만든다.
-                right = mid - 1;
-                answer = mid;
-            }
-        }
+
+
         return answer;
     }
-
 }

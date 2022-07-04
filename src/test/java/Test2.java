0f3c69247a305a5b547bfea832b17fadc22b35dc
정명한 <>
2022-07-04 11:04:39 +0900

    청소기 문제 드리더 풀었다..

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,85 +15,59 @@ public class Test2 {
      *
      * */
 
+    int[] dx = {0,1,0,-1};
+    int[] dy = {-1,0,1,0};
     int[][] answer;
     int count;
+    int N;
+
 
     public int[][] solution(int n, boolean horizontal) {
         this.answer = new int[n][n];
         this.count = 0;
+        this.N = n;
 
-        recursive(0,0, horizontal, horizontal, true);
-
-//        for(int i=0; i<n; i++){
-//            //오른쪽으로 이동
-//            int cnt = i;
-//            int cor = i+1;
-//            for(int j=0; j<n; j++){
-//                int num=0;
-//                if(cor==0){
-//                    num += 1;
-//                    cor=i;
-//                }else{
-//                    num=cnt*j;
-//                    cnt++;
-//                    cor--;
-//                }
-//                answer[i][j] = num;
-//            }
-//        }
-
+        recursive(0,0, (horizontal ? 0 : 1), horizontal);
 
         return answer;
     }
-    void recursive(int x, int y, boolean horizontal, boolean right, boolean forward){
-        if(count > Math.sqrt(answer.length)){
-            return;
-        }
+    void recursive(int x, int y, int direction, boolean right){
         answer[y][x] = ++count;
 
-        int i = forward ? 1 : -1;
-        int chk = 0;
-        if(horizontal){
-            if(x+i < 0 || x+i >= answer.length){
-                right = !right;
-                forward = !forward;
-            }else{
-                x += i;
-            }
+        for(int i=1; i<=4; i++){
+            // 오른쪽으로 먼저 돌 떄 +1 , 왼쪽으로 먼저 돌 떄 +3
+            int d = rotation(direction, right);
+            int nextX = x+dx[d];
+            int nextY = y+dy[d];
 
-            if (right) {
-                chk = answer[y+i][x];
-            } else {
-                chk = answer[y-i][x];
+            if((nextX > -1 && nextX < N) && (nextY > -1 && nextY < N)){
+                if(answer[nextY][nextX] == 0) {
+                    if(i==2){
+                        right = !right;
+                    }
+                    recursive(nextX, nextY, d, right);
+                    break;
+                }
             }
-        }else{
-            if(y+i < 0 || y+i >= answer.length){
+            // 처음 회전하는 방향에서 진행방향이 틀리면 반대로 돌아본다.
+            direction = d;
+            if(i==1){
                 right = !right;
-                forward = !forward;
-            }else{
-                y += i;
-            }
-
-            if (right) {
-                chk = answer[y][x+i];
-            } else {
-                chk = answer[y][x-i];
             }
         }
-
-        if (chk < 1) {
-            horizontal = !horizontal;
-        }
-
-        recursive(x,y,horizontal,right,forward);
     }
 
+    int rotation(int direction, boolean right){
+        return (direction + (right ? 1 : 3)) % 4;
+    }
 
     @Test
-    public void 테스트2(){
+    public void 청소기문제_테스트() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
         assertEquals(
-                new int[][]{{1,2,9,10},{4,3,8,11},{5,6,7,12},{16,15,14,13}},
-                solution(4, true)
+                objectMapper.writeValueAsString(new int[][]{{1,2,9,10},{4,3,8,11},{5,6,7,12},{16,15,14,13}}),
+                objectMapper.writeValueAsString(solution(4, true))
         );
 //        assertEquals(
 //                2+1+2,
